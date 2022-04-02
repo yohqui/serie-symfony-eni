3bce9faa2349712d57c1ae985de77943bf040b96
yohqui <yohqui@gmail.com>
2022-04-02 22:37:35 +0200

    créer les pages de base pour les séries

--- src/Controller/SerieController.php
+++ src/Controller/SerieController.php
@@ -6,10 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/series", name="serie_")
+ */
+
 class SerieController extends AbstractController
 {
     /**
-     * @Route("/series", name="serie_list")
+     * @Route("", name="list")
      */
     public function list(): Response
     {
@@ -20,16 +24,22 @@ class SerieController extends AbstractController
     }
 
     /**
-    * @Route("/series/details/{id}", name="serie_details")
-    */
+     * @Route("/details/{id}", name="details")
+     */
     public function details(int $id): Response
     {
         //todo: aller chercher la serie en bdd
         return $this->render('serie/details.html.twig', [
 
         ]);
+    }
 
-
-
+    /**
+     * @Route("/create/", name="create")
+     */
+    public function create(): Response
+    {
+        return $this->render('serie/create.html.twig', [
+        ]);
     }
 }
